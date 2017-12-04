fe7ec7de6f9cfcb16fb1c51d6bddf3039566761a
roeeyudilevitch <roeeyud@gmail.com>
2017-12-04 14:50:55 +0200

    0.1.3

--- arity.js
+++ arity.js
@@ -3,5 +3,11 @@ module.exports = function (func) {
     if (!result || !result[1] || !result[1].length) {
         return 0;
     }
-    return result[1].replace(/\{[^\}]*\}/g, 'param') .split(/\s*,\s*/).length; // eslint-disable-line
+
+    var argumentStr = result[1].trim();
+    if (argumentStr.substring(argumentStr.length - 1, argumentStr.length) === ',') {
+        argumentStr = argumentStr.substring(0, argumentStr.length - 1);
+    }
+
+    return argumentStr.replace(/\{[^\}]*\}/g, 'param') .split(/\s*,\s*/).length;
 }
--- test/arity.test.js
+++ test/arity.test.js
@@ -6,6 +6,11 @@ describe('arity', () => {
         expect(arity(testFn)).toBe(3);
     });
 
+    it('should return number of declared arguments of a function (1)', function () {
+        function testFn(a) {}
+        expect(arity(testFn)).toBe(1);
+    });
+
     it('should support default values for arguments', function () {
         function testFn(a = 1, b = 2, c) {}
         expect(arity(testFn)).toBe(3);
@@ -33,5 +38,65 @@ describe('arity', () => {
         }
 
         expect(arity(testFn)).toBe(3);
+    });
+
+    it('"," at the end', function () {
+        function testFn(
+            id,
+            options,
+            a,
+            c,
+            ) {
+            return dispatch({
+                resource: 'a',
+                method: 'b',
+                path: 'c',
+                payload: id,
+                options,
+            });
+        }
+
+        expect(arity(testFn)).toBe(4);
+    });
+
+    it('arrow', function () {
+        expect(arity((id, options, a, c) => {
+            return dispatch({
+                resource: 'a',
+                method: 'b',
+                path: 'c',
+                payload: id,
+                options,
+            });
+        })).toBe(4);
+    });
+
+
+    it('arrow fn in const', function () {
+        const fn = (id, options, a, c) => {
+            return dispatch({
+                resource: 'a',
+                method: 'b',
+                path: 'c',
+                payload: id,
+                options,
+            });
+        };
+
+        expect(arity(fn)).toBe(4);
+    });
+
+    it('function fn in const', function () {
+        const fn = function(id, options, a, c) {
+            return dispatch({
+                resource: 'a',
+                method: 'b',
+                path: 'c',
+                payload: id,
+                options,
+            });
+        };
+
+        expect(arity(fn)).toBe(4);
     });
 });
